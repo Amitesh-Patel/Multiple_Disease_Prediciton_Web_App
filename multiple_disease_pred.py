6887e45d82f3ac89a13bfb5ada6330c99f31593d
Amitesh-Patel <124021133+Amitesh-Patel@users.noreply.github.com>
2023-03-14 02:43:50 +0530

    debug

--- multiple_disease_pred.py
+++ multiple_disease_pred.py
@@ -10,11 +10,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/HP/Desktop/Multiple Disease Prediction System/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/HP/Desktop/Multiple Disease Prediction System/heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/HP/Desktop/Multiple Disease Prediction System/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 
 
